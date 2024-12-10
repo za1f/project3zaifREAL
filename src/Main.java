fba4a4aed6e07a3665e978678b50862ea85e549d
zaif <zaifbh4708@gmail.com>
2024-12-09 21:54:39 -0500

    idk

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        People people = new People();
+        people.KassIno();
+        System.out.println(people);
+    }
+}
---
+++ src/People.java
@@ -0,0 +1,29 @@
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
+public class People {
+    private String name;
+    private int age;
+    private String desc;
+    private String weakness;
+    private String strengths;
+    private String gender;
+
+    public People() {
+    }
+
+    public void KassIno(){
+        name = "Kass Ino";
+        gender = "F";
+        age = 19;
+        desc = "Crazy New Jeans fan who spent too much money on their concerts and albums";
+        weakness = "she cannot tolerate alcohol";
+        strengths = "she never gives up";
+    }
+
+    public void UncleTom(){
+
+    }
+
+
+
+}
